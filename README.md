c53359241ed0c184074cadbaaeef4144563d5aaf
Shunta Saito <shunta@preferred.jp>
2019-09-26 13:37:46 +0900

    Deploy to GitHub Pages

--- README.md
+++ README.md
@@ -41,3 +41,9 @@ https://japan-medical-ai.github.io/medical-ai-course-materials/
 docker pull mitmul/medical-ai-course-materials
 docker run -v "$PWD:/medical" --rm -t mitmul/medical-ai-course-materials bash -c "cd /medical && bash build.sh"
 ```
+
+## デプロイ
+
+```bash
+GH_TOKEN=<YOUR_GITHUB_TOKEN> bash deploy.sh
+```
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-git config user.email "example@example.com"
-git config user.name "Travis-CI"
+git config user.email "shunta@preferred.jp"
+git config user.name "Shunta Saito"
 
 git add -A
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@github.com/mitmul/medical-ai-course-materials.git" master:master
+git push --force --quiet "https://${GH_TOKEN}@github.com/mitmul/medical-ai-course-materials.git" master:gh-pages
