2e4c05fe5f0f5dd27c16161d49b611957c71c6c5
Shunta Saito <shunta@preferred.jp>
2019-09-26 14:46:33 +0900

    Fix CI settings

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ URLBASE="https://japan-medical-ai.github.io/medical-ai-course-materials/notebook
 OPEN_IN_COLAB_PREFIX="https://colab.research.google.com/github/japan-medical-ai/medical-ai-course-materials"
 NOTEBOOK_DIR="notebooks"
 
+git clone https://github.com/japan-medical-ai/medical-ai-course-materials
+cd medical-ai-course-materials
+
 rm -rf docs
 mkdir docs && touch docs/.nojekyll
 
@@ -24,3 +27,10 @@ for fn in $(find docs/notebooks -name "*.html");
 do
     python3 scripts/insert_colab_link.py $fn ${OPEN_IN_COLAB_PREFIX} ${NOTEBOOK_DIR}
 done
+
+git config user.email "shunta@preferred.jp"
+git config user.name "Shunta Saito"
+
+git add -A
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@github.com/mitmul/medical-ai-course-materials.git" master:master
