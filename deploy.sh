133dd7f069b81a359bcd81a5a5c0447d0603f569
Shunta Saito <shunta.saito@gmail.com>
2019-11-19 13:35:34 +0900

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git config user.email "shunta@preferred.jp"
+git config user.name "Shunta Saito"
+
+git add -A
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@github.com/mitmul/medical-ai-course-materials.git" master:master
